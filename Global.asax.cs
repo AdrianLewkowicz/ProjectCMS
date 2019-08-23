46c75d1dff3be5d1794b23018e0938ed1a7b2e80
AdrianLewkowicz <48416578+AdrianLewkowicz@users.noreply.github.com>
2019-08-23 19:59:27 +0200

    Add files via upload

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using CmsShop.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,33 @@ namespace CmsShop
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AuthenticateRequest()
+        {
+            if(User == null)
+            {
+                return;
+            }
+            // Pobieramy nazwe uzytkownika
+            string username = Context.User.Identity.Name;
+
+            // deklarujmey tablecie z rolami
+            string[] roles = null;
+
+            using (DB db = new DB())
+            {
+                // pobieramy dane dla uzytkownika z bazdy aby pobrać role
+                UserDTO dto = db.Users.FirstOrDefault(x => x.UserName.Equals(username));
+                roles = db.UserRoles.Where(x => x.UserId == dto.Id).Select(x => x.Role.Name).ToArray();
+            }
+
+            // tworzymy IPrincpla object
+            IIdentity userIdentity = new GenericIdentity(username);
+            IPrincipal newUserObj = new GenericPrincipal(userIdentity, roles);
+
+            // update Context.User
+            Context.User = newUserObj;
         }
     }
 }
